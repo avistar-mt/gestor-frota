323bf5580fdd30952869f7838cb4df48816a32b2
airtonsouza <airkun.2014@gmail.com>
2024-07-25 18:51:46 -0300

    fix: report blade

--- resources/views/operation/reservation/report.blade.php
+++ resources/views/operation/reservation/report.blade.php
@@ -1,6 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
+    <nav class="navbar navbar-main navbar-expand-lg  px-0 mx-4 shadow-none border-radius-xl z-index-sticky " id="navbarBlur"
+        data-scroll="false">
+        <div class="container-fluid py-1 px-3">
+            @include('layouts.navbars.auth.topnav', ['title' => 'Relatórios de Reservas'])
+            <div class="sidenav-toggler sidenav-toggler-inner d-xl-block d-none ">
+                <a href="javascript:;" class="nav-link p-0">
+                    <div class="sidenav-toggler-inner">
+                        <i class="sidenav-toggler-line bg-white"></i>
+                        <i class="sidenav-toggler-line bg-white"></i>
+                        <i class="sidenav-toggler-line bg-white"></i>
+                    </div>
+                </a>
+            </div>
+            <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+                <div class="ms-md-auto pe-md-3 d-flex align-items-center">
+                    <div class="input-group">
+                        <span class="input-group-text text-body"><i class="fas fa-search" aria-hidden="true"></i></span>
+                        <input type="text" class="form-control" placeholder="Type here...">
+                    </div>
+                </div>
+                <ul class="navbar-nav  justify-content-end">
+                    <li class="nav-item d-flex align-items-center">
+                        @include('auth.logout')
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- End Navbar -->
+
 <div class="container-fluid py-4">
     <div class="row mt-4">
         <form action="{{ route('reservation-generateReport') }}" method="POST">
@@ -116,14 +146,6 @@ if (document.getElementById('choices-users')) {
     var user = document.getElementById('choices-users');
     const example = new Choices(user);
 }
-
-
-// if (document.querySelector('.datetimepicker')) {
-//     flatpickr('.datetimepicker', {
-//         allowInput: true,
-//         dateFormat: "d/m/Y",
-//     }); // flatpickr
-// }
 
 
 document.addEventListener("DOMContentLoaded", function(){
