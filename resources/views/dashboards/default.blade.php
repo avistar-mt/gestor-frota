e981cf7155452959ec1b2d4aae9acbd1822f7107
airtonsouza <airkun.2014@gmail.com>
2024-08-06 16:42:17 -0400

    fix sidevar nav

--- resources/views/dashboards/default.blade.php
+++ resources/views/dashboards/default.blade.php
@@ -25,12 +25,22 @@
                     <li class="nav-item d-flex align-items-center">
                         @include('auth.logout')
                     </li>
+
+                    <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                        <a href="javascript:;" class="nav-link text-white p-0" id="iconNavbarSidenav">
+                            <div class="sidenav-toggler-inner">
+                                <i class="sidenav-toggler-line bg-white"></i>
+                                <i class="sidenav-toggler-line bg-white"></i>
+                                <i class="sidenav-toggler-line bg-white"></i>
+                            </div>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
     <!-- End Navbar -->
-     
+
     <div class="container-fluid py-4">
         <div class="row">
             <div class="col-lg-12">
